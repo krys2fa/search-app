3f59ee1d70907812e0640efa96438f9efeb18817
Christopher Amanor <christopher.k.amanor@gmail.com>
2023-09-08 12:04:00 +0000

    Replace default favicon, start project structure and create SearchBar component

---
+++ src/containers/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter name..." />
+    </div>
+  );
+}
+
+export default SearchBar;
