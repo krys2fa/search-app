e116d00e2e21b2372a02de43d2c9155650127aed
Christopher Amanor <christopher.k.amanor@gmail.com>
2023-09-08 12:08:46 +0000

    Add handleChange function

--- src/containers/SearchBar/SearchBar.jsx
+++ src/containers/SearchBar/SearchBar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-function SearchBar() {
+const handleChange = (event) => {
+  console.log(event.target.value);
+};
+
+const SearchBar = () => {
   return (
     <div>
-      <input type="text" placeholder="Enter name..." />
+      <input type="text" placeholder="Enter name..." onChange={handleChange} />
     </div>
   );
-}
+};
 
 export default SearchBar;
